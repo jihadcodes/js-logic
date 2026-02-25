2595ae8b6510d8ef6f9511cc15c911cdd87dca86
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-26 00:51:13 +0600

    method

--- script.js
+++ script.js
@@ -1,13 +1,63 @@
+// Filter
+const names = ["Jihad","Rakib","Jahid","Saim"];
+// const newarr = names.filter(function(){
+//     return true;
+// })
+// const newarr2 = names.filter(function(){
+//     return false;
+// })
+
+// let newarr3 = names.filter(function(value){
+//     if (value === "Jahid") {
+//         return true;        
+//     } 
+// })
+
+// let newarr4 = names.filter(function(value){
+//     if (value.startsWith('J')) {
+//         return true;
+//     }
+// })
 
 
-setTimeout(() => {
-    alert("Hello world")
-}, 2000);
 
-let age = 18 ;
 
-if (age >= 18) {
-    console.log("You are adult");
+
+// Map
+// const names = ["Jihad","Rakib","Jahid","Saim"];
+// let newarr =names.map(function(value){
+//     return value + " Hossain";
+// });
+
+
+
+
+
+// For each
+// let names = ["Jihad","Rkib","Jahid"];
+
+// names.forEach(function(value){
+//     if(value === "Jihad") {}
+//     else{
+//          console.log(value);
+//     }
+     
+// })
+
+
+
+
+
+
+
+// setTimeout(() => {
+//     alert("Hello world")
+// }, 2000);
+
+// let age = 18 ;
+
+// if (age >= 18) {
+//     console.log("You are adult");
     
     
-}
+// }
