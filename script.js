11a89632dd95abebee2b00141e7bef081c62cb5b
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-28 23:17:14 +0600

    filebtn

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ btn.addEventListener('click', function(){
     fileinp.click();
 })
 
-
+fileinp.addEventListener('change', function(dets){
+    btn.textContent = dets.target.files[0].name;
+    
+})
 
 
 // Change event
